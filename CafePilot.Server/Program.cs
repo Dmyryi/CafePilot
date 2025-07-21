e07b238002e637743460b71f55c09c9c6b76aefd
Dmytryi <muzalevskyid333@gmail.com>
2025-07-21 13:21:22 +0300

    CAF-5 Розроблення фронтенд частини

--- CafePilot.Server/Program.cs
+++ CafePilot.Server/Program.cs
@@ -3,27 +3,43 @@ using CafePilot.Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Название политики CORS
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
+// Добавляем сервисы в контейнер
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddScoped<ICafeService, CafeService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IMenuService, MenuService>();
 builder.Services.AddOpenApi();
 
+// Конфигурируем CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("https://localhost:65382") // фронтенд адрес, разрешенный для запросов
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
+// Настройка middleware
 app.UseDefaultFiles();
 app.MapStaticAssets();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
+
+// Важно: используем CORS перед авторизацией и контроллерами
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
